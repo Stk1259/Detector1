eb8a0ec78ad6e8b3cef23926f41ea4335234e48c
Stk1259 <stk01012010@gmail.com>
2022-04-27 19:48:36 +0500

    init

--- app/src/main/java/com/example/detector/MainActivity.kt
+++ app/src/main/java/com/example/detector/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.detector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text1: TextView = findViewById<TextView>(R.id.text1)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            text1.text = "Вы точно гей!"
+        }
     }
 }
+
+
